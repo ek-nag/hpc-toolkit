ba19298e29124db7b27118a55c4d69f1ac5266d7
Eimantas Kazakevicius <117852832+ek-nag@users.noreply.github.com>
2024-04-15 18:40:13 +0100

    Update image.py
    Updating path to credentials file

--- community/front-end/ofe/website/ghpcfe/cluster_manager/image.py
+++ community/front-end/ofe/website/ghpcfe/cluster_manager/image.py
@@ -172,7 +172,7 @@ deployment_groups:
         try:
             logger.info(f"Invoking ghpc create for the image {self.image.id}")
             env = os.environ.copy()
-            env['GOOGLE_APPLICATION_CREDENTIALS'] = self._get_credentials_file()
+            env['GOOGLE_APPLICATION_CREDENTIALS'] = target_dir / "cloud_credentials"
             log_out_fn = target_dir / "ghpc_create_log.stdout"
             log_err_fn = target_dir / "ghpc_create_log.stderr"
             with log_out_fn.open("wb") as log_out:
